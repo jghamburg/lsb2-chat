e6e1eee63c7b0c2c4ec81a08ceadee7925786d6f
Jörg Gellien <joerg.gellien@otto.de>
2018-08-07 12:48:32 +0200

    fix(config): fix service references in local profile

--- docker/run.sh
+++ docker/run.sh
@@ -3,4 +3,16 @@ profile="$1:-local"
 java_heap="${2:-768m}"
 
 echo "startup the container! find log in docker logs"
-java -Djava.security.egd=file:/dev/./urandom -server -Xms${java_heap} -Xmx${java_heap} -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=4242,suspend=n -Dspring.profiles.active=${profile} -jar /app/app.jar
+if [ -z ${JAVA_REMOTE_DEBUGGING+x} ]; then
+  echo "starting container without debugging options"
+  java -Djava.security.egd=file:/dev/./urandom \
+    -server -Xms${java_heap} -Xmx${java_heap} \
+    -Dspring.profiles.active=${profile} -jar /app/app.jar
+else
+  echo "starting container, internal debug port is 4242"
+  java -Djava.security.egd=file:/dev/./urandom \
+    -server -Xms${java_heap} -Xmx${java_heap} \
+    -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=4242,suspend=n \
+    -Dspring.profiles.active=${profile} -jar /app/app.jar
+fi
+# The End
