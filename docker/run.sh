b87272dec17099e01cd623f6f261d88eff84554d
Jörg Gellien <joerg.gellien@otto.de>
2018-08-19 20:44:00 +0200

    feat(debug): add config for remote debugging

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 profile="$1"
 java_heap="${2:-768m}"
+debug_port="${DEBUG_PORT:49999}"
 
 echo "startup the container! find log in docker logs"
 if [ -z ${JAVA_REMOTE_DEBUGGING+x} ]; then
@@ -12,7 +13,7 @@ else
   echo "starting container, internal debug port is 4242"
   java -Djava.security.egd=file:/dev/./urandom \
     -server -Xms${java_heap} -Xmx${java_heap} \
-    -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=4242,suspend=n \
+    -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=${debug_port},suspend=n \
     -Dspring.profiles.active=${profile} -jar /app/app.jar
 fi
 # The End
